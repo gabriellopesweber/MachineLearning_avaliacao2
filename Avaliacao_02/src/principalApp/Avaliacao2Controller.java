1e328ebb9b73e326aed44fbe67bc7a420b7dcf93
Rafael Fagundes <rafaelfagundes64@gmail.com>
2020-10-29 21:39:16 -0300

    Subindo nova versao

--- Avaliacao_02/src/principalApp/Avaliacao2Controller.java
+++ Avaliacao_02/src/principalApp/Avaliacao2Controller.java
@@ -3,19 +3,16 @@ package principalApp;
 import java.io.File;
 import java.text.DecimalFormat;
 
-import javax.swing.JOptionPane;
-
 import extrairCaracteristicas.ExtractCaracteristicas;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.media.AudioClip;
+import javafx.scene.control.TextField;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 import utils.Classificacao_caracteristicas;
+import weka.classifiers.functions.MultilayerPerceptron;
 
 public class Avaliacao2Controller {
 
@@ -23,53 +20,54 @@ public class Avaliacao2Controller {
 	private MediaView originalAudio;
 
 	@FXML
-	private Label resultado_PretoFardaChief;
+	private Label classificaCat;
+
 	@FXML
-	private Label resultado_AzulFardaChief;
+	private Label classificaDog;
+
 	@FXML
-	private Label resultado_Camisa_Grampa;
+	private TextField txtTempoTreinamento;
+
 	@FXML
-	private Label resultado_Barba_Grampa;
-	@FXML
-	private Label classificaChief;
-	@FXML
-	private Label classificaGrampa;
+	private TextField txtTaxaAprendizagem;
 
 	File f;
 	double[] caracteristicasExtraidas;
 	DecimalFormat formatador = new DecimalFormat("0.00");
 
 	@FXML
-	public void extrairCaracteristicas() {
-		Object[] options_JO = { "Não Realizar extração", "Realizar extração" };
+	public void executaConfiguracao() {
+		if (f != null) {
+			MultilayerPerceptron mp = new MultilayerPerceptron();
+			mp.setTrainingTime(Integer.parseInt(txtTempoTreinamento.getText()));
+			mp.setLearningRate(Integer.parseInt(txtTaxaAprendizagem.getText()));
 
-		int optionSelecionada = JOptionPane.showOptionDialog(null, "Deseja Realizar a extração?", "Aviso",
-				JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options_JO, options_JO[0]);
-
-		if (optionSelecionada == 1) {
-			ExtractCaracteristicas.extrair(originalAudio);
+			System.out.println("TT -> " + mp.getTrainingTime());
+			System.out.println("LR -> " + mp.getLearningRate());
+			
+			
 		}
 	}
 
 	@FXML
 	public void selecionaAudio() {
-		f = buscaImg();
+		f = buscaAudio();
 		if (f != null) {
 			Media arquivoAudio = new Media(f.toURI().toString());
 			MediaPlayer MPaudio = new MediaPlayer(arquivoAudio);
 			getOriginalAudio().setMediaPlayer(MPaudio);
-//			getOriginalAudio().setFitWidth(img.getWidth());
-//			getOriginalAudio().setFitHeight(img.getHeight());
+
+			// caracteristicasExtraidas = ExtractCaracteristicas.extraiCaracteristicas(f,
+			// originalAudio);
+
 			caracteristicasExtraidas = ExtractCaracteristicas.extraiCaracteristicas(f, originalAudio);
-			resultado_PretoFardaChief.setText(formatador.format(caracteristicasExtraidas[0]));
-			resultado_AzulFardaChief.setText(formatador.format(caracteristicasExtraidas[1]));
-			resultado_Barba_Grampa.setText(formatador.format(caracteristicasExtraidas[2]));
-			resultado_Camisa_Grampa.setText(formatador.format(caracteristicasExtraidas[3]));
+
+			MPaudio.play();
 		}
 	}
 
 	@FXML
-	public void classificarImagem() {
+	public void classificarAudio() {
 		if (f != null) {
 			double[] caracteristicasClassificada = Classificacao_caracteristicas.naiveBayes(caracteristicasExtraidas);
 			double[] caracPor = new double[caracteristicasClassificada.length];
@@ -78,21 +76,15 @@ public class Avaliacao2Controller {
 				caracPor[cont] = d * 100;
 				cont++;
 			}
-			classificaChief.setText(formatador.format(caracPor[0]) + "%");
-			classificaGrampa.setText(formatador.format(caracPor[1]) + "%");
+			classificaCat.setText(formatador.format(caracPor[0]) + "%");
+			classificaDog.setText(formatador.format(caracPor[1]) + "%");
 		}
 	}
-	
-	@FXML
-	public void executaJ48() {
-		
-	}
 
-	private File buscaImg() {
+	private File buscaAudio() {
 		FileChooser fileChooser = new FileChooser();
-		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Imagens", "*.jpg", "*.JPG", "*.png",
-				"*.PNG", "*.gif", "*.GIF", "*.bmp", "*.BMP"));
-		fileChooser.setInitialDirectory(new File("imagensParaTeste"));
+		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Audio", "*.wav", "*.mp3"));
+		// fileChooser.setInitialDirectory(new File("imagensParaTeste"));
 		File imgSelec = fileChooser.showOpenDialog(null);
 		try {
 			if (imgSelec != null) {
---
+++ Avaliacao_02/src/utils/mp.java
@@ -0,0 +1,42 @@
+package utils;
+
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.trees.J48;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class mp {
+
+	public static double[] multilayerPerceptron(double[] caracteristicas) {
+		double[] retorno = { 0, 0 };
+		try {
+			// *******carregar arquivo de características
+			DataSource ds = new DataSource("baseParaConhecimento\\caracteristicasAudio.arff");
+			Instances instancias = ds.getDataSet();
+			instancias.setClassIndex(instancias.numAttributes() - 1);
+
+			// cria a árvore
+			MultilayerPerceptron arvore = new MultilayerPerceptron();
+			arvore.buildClassifier(instancias);
+			// arvore.setUnpruned(true);//sem podas
+
+			// Novo registro
+			Instance novo = new DenseInstance(instancias.numAttributes());
+			novo.setDataset(instancias);
+			novo.setValue(0, caracteristicas[0]);
+			novo.setValue(1, caracteristicas[1]);
+			novo.setValue(2, caracteristicas[2]);
+			novo.setValue(3, caracteristicas[3]);
+			novo.setValue(4, caracteristicas[4]);
+			novo.setValue(5, caracteristicas[5]);
+
+			retorno = arvore.distributionForInstance(novo);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return retorno;
+	}
+}
